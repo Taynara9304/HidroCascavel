51a895f76c31f804a3b32f57a82100e2e1a70977
Lucas <lukinhasx248@gmail.com>
2025-08-28 17:49:02 +0000

    fiz

--- hidroCascavel/app/Secoes/CarosselInicial.jsx
+++ hidroCascavel/app/Secoes/CarosselInicial.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { View } from 'react-native';
+import { NavBar } from '..//componentes/NavBar'
 
 const CarrosselInicial = () => {
     return (
         <View>
-            
+            <NavBar></NavBar>
         </View>
     )
 }
+
+export default CarrosselInicial;
